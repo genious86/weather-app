b9fdb5d080ecf7c54212d777355c38c2a2bbc299
genious86 <pauleugenius86@gmail.com>
2024-11-12 19:39:05 +0300

    filling in tthe display error function

--- script.js
+++ script.js
@@ -5,6 +5,17 @@ const api_key = "ddaf05d18883b4b53664ed4f6dca9f0e"
 
 weatherform.addEventListener("submit", event =>{
 
+    event.preventDefault()
+
+    const city =cityinput.value
+
+    if(city) {
+
+    }
+
+    else{
+        displayError("Please Enter a City")
+    }
 })
 
 async function getWeatherData(city) {
@@ -20,5 +31,10 @@ function getWeatherEmoji(weatherId){
 }
 
 function displayError(message){
-    
+
+    const Error_display =document.createElement("p")
+    Error_display.textContent = message
+    Error_display.classList.add("Error_display")
+
+    card.textContent = ""
 }
