5ca40441507c251c7ca8d41f5ca66f22c42fcfca
genious86 <pauleugenius86@gmail.com>
2024-11-12 19:04:15 +0300

    declaring variables

---
+++ script.js
@@ -0,0 +1,4 @@
+const weatherform =document.querySelector(".weatherfm")
+const cityinput = document.querySelector(".cityinput")
+const card = document.querySelector(".card")
+const api_key = "ddaf05d18883b4b53664ed4f6dca9f0e"
