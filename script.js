69c8c296ce9afddb6d7dc3df6810bd86cddae9d3
genious86 <pauleugenius86@gmail.com>
2024-11-12 19:15:08 +0300

    declaring the needed functions

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ const weatherform =document.querySelector(".weatherfm")
 const cityinput = document.querySelector(".cityinput")
 const card = document.querySelector(".card")
 const api_key = "ddaf05d18883b4b53664ed4f6dca9f0e"
+
+weatherform.addEventListener("submit", event =>{
+
+})
+
+async function getWeatherData(city) {
+    
+}
+
+function displayWeatherInfo(data){
+
+}
+
+function getWeatherEmoji(weatherId){
+
+}
+
+function displayError(message){
+    
+}
