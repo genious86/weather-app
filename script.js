e93c3d6d32294685b14c0afad441e858c45e710d
genious86 <pauleugenius86@gmail.com>
2024-11-13 11:38:07 +0300

    adding content to card and appending

--- script.js
+++ script.js
@@ -50,6 +50,31 @@ const {name: city,
 
 card.textContent = ""
 card.style.display ="flex"
+
+const cityDisplay =document.createElement("h1")
+const tempDisplay =document.createElement("p")
+const humidityDisplay =document.createElement("p")
+const descriptionDisplay =document.createElement("p")
+const weatherEmoji =document.createElement("p")
+
+cityDisplay.textContent = city
+tempDisplay.textContent =`${(temp - 273.15).toFixed(1)}°C`
+humidityDisplay.textContent =`Humidity: ${humidity}%`
+descriptionDisplay.textContent = description
+weatherEmoji.textContent = getWeatherEmoji(id)
+
+cityDisplay.classList.add("citydisplay")
+tempDisplay.classList.add("tempdisplay")
+descriptionDisplay.classList.add("description")
+weatherEmoji.classList.add("Weather_emoji")
+
+card.appendChild(cityDisplay)
+card.appendChild(tempDisplay)
+card.appendChild(humidityDisplay)
+card.appendChild(descriptionDisplay)
+card.appendChild(weatherEmoji)
+
+
 }
 
 function getWeatherEmoji(weatherId){
